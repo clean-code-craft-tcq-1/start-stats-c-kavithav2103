ee756726b1dc44f9d88038a1f9274c456b5614d2
kavithav2103 <77490308+kavithav2103@users.noreply.github.com>
2021-01-27 14:35:56 +0530

    Changes made for compilation issue

--- stats.c
+++ stats.c
@@ -1,8 +1,20 @@
 #include "stats.h"
 
+struct Stats 
+{
+	float average;
+	float min;
+	float max;
+}s;
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
     s.average = 0;
+    for (int i =0 ;i<setlength;i++)
+   {
+	 s.average = s.average + numberset[i];
+   }
+   
+   s.average = s.average /setlength;
+   
     s.min = 0;
     s.max = 0;
 }
